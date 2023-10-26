0be6d676bc59713c3f0ddd1d1e1c65868ed4a238
luissalexx <lui150803@gmail.com>
2023-10-26 01:12:21 -0600

    tercer commit

--- src/EzcodeApp.jsx
+++ src/EzcodeApp.jsx
@@ -1,12 +1,11 @@
-import { NavAuth } from "./ezcode/components/NavAuth"
-//import { ChangeNav } from "./ezcode/components/changeNav"
+import { ChangeNav } from "./ezcode/components/changeNav"
 import { AppRouter } from "./router/AppRouter"
 import { AppTheme } from "./theme"
 
 export const EzcodeApp = () => {
   return (
     <AppTheme>
-      <NavAuth />
+      <ChangeNav />
       <AppRouter />
     </AppTheme>
   )
