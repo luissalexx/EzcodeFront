7691a3ece1c2ba165b4457149a5b18b9bc722c2e
luissalexx <105310096+luissalexx@users.noreply.github.com>
2024-01-22 18:20:23 -0600

    final commit

--- src/ezcode/pages/curso/components/TareaEdit.jsx
+++ src/ezcode/pages/curso/components/TareaEdit.jsx
@@ -1,36 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { Grid, TextField, Typography, Button, TextareaAutosize } from "@mui/material"
-import { ChangeCursoNav } from '../../../components/ChangeCursoNav'
-import { useNavigate, useParams } from 'react-router-dom';
-import ezcodeApi from '../../../../api/ezcodeApi';
-import Swal from 'sweetalert2';
+import { useNavigate, useParams } from "react-router-dom"
+import { ChangeCursoNav } from "../../../components/ChangeCursoNav";
+import { Button, FormControl, FormControlLabel, Grid, Radio, RadioGroup, TextField, TextareaAutosize, Typography } from "@mui/material";
+import ezcodeApi from "../../../../api/ezcodeApi";
+import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 
-export const TareaEdit = () => {
-    const { id, tareaId } = useParams();
+export const TemaEdit = () => {
+    const { id, idTema } = useParams();
     const navigate = useNavigate();
+    const [categoria, setCategoria] = useState('');
 
     const [formData, setFormData] = useState({
         nombre: '',
-        asignacion: ''
+        contenido: '',
+        url: '',
+        precio: 0,
     });
 
     useEffect(() => {
         const fetchUserData = async () => {
             try {
-                const response = await ezcodeApi.get(`curso/tarea/${id}/${tareaId}`);
-                const tareaDataFromServer = response.data;
+                const response = await ezcodeApi.get(`curso/tema/${id}/${idTema}`);
+                const temaDataFromServer = response.data.temaEncontrado;
 
-                if (tareaDataFromServer) {
+                if (temaDataFromServer) {
+
+                    if (temaDataFromServer.url) {
+                        setCategoria('Url')
+                    } else if (temaDataFromServer.contenido) {
+                        setCategoria('Contenido')
+                    }
 
                     setFormData({
-                        nombre: tareaDataFromServer.nombre || '',
-                        asignacion: tareaDataFromServer.asignacion || '',
+                        nombre: temaDataFromServer.nombre || '',
+                        precio: temaDataFromServer.precio || '',
+                        contenido: temaDataFromServer.contenido || '',
+                        url: temaDataFromServer.url || '',
                     });
                 } else {
-                    console.error('No se encontraron tareas en la respuesta del servidor');
+                    console.error('No se encontraron temas en la respuesta del servidor');
                 }
             } catch (error) {
-                console.error('Error al obtener datos de tarea:', error);
+                console.error('Error al obtener datos de tema:', error);
             }
         };
 
@@ -41,26 +52,53 @@ export const TareaEdit = () => {
         e.preventDefault();
 
         try {
-            await ezcodeApi.put(`curso/asignacion/${id}/${tareaId}`, formData);
-            Swal.fire({
-                title: 'Datos actualizados con éxito',
-                icon: 'success',
-                confirmButtonText: 'Ok',
-            }).then((result) => {
-                if (result.isConfirmed) {
+            if (categoria === 'Url') {
+                if (formData.url.includes('https://drive.google.com/file/d/')) {
+                    await ezcodeApi.put(`curso/tema/${id}/${idTema}`, formData);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success',
+                        text: 'Tema editado exitosamente!',
+                        confirmButtonText: 'Ok'
+                    });
                     navigate(-1);
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Solo se permiten url embebidos de drive',
+                        confirmButtonText: 'Ok'
+                    });
                 }
-            });
+            }
+
+            if (categoria === 'Contenido') {
+                await ezcodeApi.put(`curso/tema/${id}/${idTema}`, formData);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'Tema editado exitosamente!',
+                    confirmButtonText: 'Ok'
+                });
+                navigate(-1);
+            }
         } catch (error) {
-            console.log(error)
+            console.log(error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Hubo un problema al editar el tema. Inténtelo de nuevo.',
+                confirmButtonText: 'Ok'
+            });
         }
     };
 
-    const handleInputChange = (e) => {
+    const handleChange = (e) => {
         const { name, value } = e.target;
+
         setFormData((prevFormData) => ({
             ...prevFormData,
-            [name]: value
+            [name]: name === 'precio' ? parseFloat(value) : value,
         }));
     };
 
@@ -75,6 +113,7 @@ export const TareaEdit = () => {
                 justifyContent="center"
                 sx={{ minHeight: '100vh', padding: 4 }}
             >
+
                 <Grid item
                     className='box-shadow'
                     xs={3}
@@ -85,40 +124,67 @@ export const TareaEdit = () => {
                         borderRadius: 2
                     }}>
 
-                    <Typography variant='h5' sx={{ mb: 1 }}>Editar Tarea</Typography>
-                    <br />
+                    <Typography variant='h5' sx={{ mb: 1 }}>Editar Tema</Typography>
+                    <p>Los temas que no tengan precio pasaran a ser como temas pagados, los temas pagados no se pueden borrar ni editar, los temas no pueden cambiar de categoria en su contenido</p>
                     <form onSubmit={handleFormSubmit}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    autoComplete='off'
-                                    label="Nombre"
-                                    variant="outlined"
-                                    fullWidth
-                                    id="nombre"
-                                    name="nombre"
-                                    required
-                                    value={formData.nombre}
-                                    onChange={handleInputChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
+                        <TextField
+                            label="Nombre"
+                            name="nombre"
+                            value={formData.nombre}
+                            fullWidth
+                            margin="normal"
+                            autoComplete='off'
+                            onChange={handleChange}
+                            required
+                        />
+                        {categoria === 'Contenido' ? (
+                            <FormControl fullWidth>
+                                <label>Texto</label>
                                 <TextareaAutosize
-                                    id="asignacion"
-                                    name="asignacion"
+                                    onChange={handleChange}
+                                    name="contenido"
                                     color="neutral"
+                                    value={formData.contenido}
                                     minRows={10}
                                     maxRows={10}
-                                    value={formData.asignacion}
-                                    onChange={handleInputChange}
-                                    style={{ fontFamily: 'Arial', fontSize: '16px', resize: 'none', width: '100%' }}
-                                    placeholder='Escribe aqui la asignacion'
-                                    required />
-                            </Grid>
-                        </Grid>
+                                    style={{
+                                        fontFamily: 'Arial',
+                                        fontSize: '16px',
+                                        resize: 'none',
+                                        width: '100%',
+                                    }}
+                                    required
+                                />
+                            </FormControl>
+                        ) : null}
+                        {categoria === 'Url' ? (
+                            <TextField
+                                label="Url"
+                                name="url"
+                                type="url"
+                                value={formData.url}
+                                fullWidth
+                                autoComplete='off'
+                                onChange={handleChange}
+                                required
+                                helperText="Por favor introduce un url valido"
+                            />
+                        ) : null}
+                        <TextField
+                            label="Precio"
+                            name="precio"
+                            type='number'
+                            value={formData.precio}
+                            fullWidth
+                            margin="normal"
+                            autoComplete='off'
+                            onChange={handleChange}
+                            required
+                        />
                         <br />
+                        <hr />
                         <Button type="submit" variant="contained" color="primary">
-                            Editar Tarea
+                            Actualizar Tema
                         </Button>
                         <Button onClick={() => navigate(-1)} color="primary">
                             Volver
@@ -129,3 +195,4 @@ export const TareaEdit = () => {
         </div>
     )
 }
+
